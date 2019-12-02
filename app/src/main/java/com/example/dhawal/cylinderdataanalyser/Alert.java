3f0dbf5ca9422a825226a53ec697035449f2976d
Dhawal <dhawalsharma26@icloud.com>
2019-12-03 03:26:40 +0530

    almost done

--- app/src/main/java/com/example/dhawal/cylinderdataanalyser/Alert.java
+++ app/src/main/java/com/example/dhawal/cylinderdataanalyser/Alert.java
@@ -2,12 +2,36 @@ package com.example.dhawal.cylinderdataanalyser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Alert extends AppCompatActivity {
-
+public class Alert extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
+        String[] percent = {"10%", "15%", "20%", "25%", "30%"};
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
+        spinner=findViewById(R.id.alertspinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,percent);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(getApplicationContext(),percent[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
